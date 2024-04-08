1ad4fd2d873046068b44f4a1a612d97f3d2136b8
Mario <cursotecnoweb@gmail.com>
2024-04-08 20:57:04 +0200

    Updated Admin form view

--- view/admin/form_view.php
+++ view/admin/form_view.php
@@ -2,19 +2,19 @@
     <div class="row mb-3">
         <label class="col-sm-2 col-form-label" for="user_name">User:</label>
         <div class="col-sm-8">
-            <input class="form-control" type="text" name="user_name" id="user_name" value="<?php if(isset($fields['user_name'])) echo $fields['user_name']; ?>">
+            <input class="form-control" type="text" name="user_name" id="user_name" value="<?php if(isset($fields['user_name'])) echo $fields['user_name']; ?>" required>
         </div>                
     </div>
     <div class="row mb-3">
         <label class="col-sm-2 col-form-label" for="password">Password:</label>
         <div class="col-sm-8">
-            <input class="form-control" type="password" name="password" id="password" value="<?php if(isset($fields['password'])) echo $fields['password']; ?>">
+            <input class="form-control" type="password" name="password" id="password" value="<?php if(isset($fields['password'])) echo $fields['password']; ?>" required>
         </div>                
     </div>
     <div class="row mb-3">
         <label class="col-sm-2  col-form-label" for="email">Email:</label>
         <div class="col-sm-8">
-            <input class="form-control" type="email" name="email" id="email" value="<?php if(isset($fields['email'])) echo $fields['email']; ?>">
+            <input class="form-control" type="email" name="email" id="email" value="<?php if(isset($fields['email'])) echo $fields['email']; ?>" required>
         </div>                
     </div>               
     <div class="row mb-3">
