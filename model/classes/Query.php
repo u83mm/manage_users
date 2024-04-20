eebc46993a0aa91df60c0104d11af3b1317f0bb5
Mario <cursotecnoweb@gmail.com>
2024-04-20 15:35:15 +0200

    Updated Query.php class

--- model/classes/Query.php
+++ model/classes/Query.php
@@ -11,64 +11,86 @@
         
         public function selectAllBy(string $table, string $field, string $value, object $dbcon): array  
         {
-            $query = "SELECT * FROM $table WHERE $field = :val";                         
+            $query = "SELECT * FROM $table WHERE $field = :val";                                    
 
-            $stm = $dbcon->pdo->prepare($query);
-            $stm->bindValue(":val", $value);                            
-            $stm->execute();       
-            $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
-            $stm->closeCursor();
+            try {
+                $stm = $dbcon->pdo->prepare($query);
+                $stm->bindValue(":val", $value);                            
+                $stm->execute();       
+                $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
+                $stm->closeCursor();
+    
+                return $rows;
 
-            return $rows;
+            } catch (\Throwable $th) {
+                throw new \Exception("{$th->getMessage()}", 1);
+            }
         }
 
         public function selectOneBy(string $table, string $field, string $value, object $dbcon): array  
         {
-            $query = "SELECT * FROM $table WHERE $field = :val";                         
+            $query = "SELECT * FROM $table WHERE $field = :val";                                     
 
-            $stm = $dbcon->pdo->prepare($query);
-            $stm->bindValue(":val", $value);                            
-            $stm->execute();       
-            $rows = $stm->fetch(PDO::FETCH_ASSOC);
-            $stm->closeCursor();
+            try {
+                $stm = $dbcon->pdo->prepare($query);
+                $stm->bindValue(":val", $value);                            
+                $stm->execute();       
+                $rows = $stm->fetch(PDO::FETCH_ASSOC);
+                $stm->closeCursor();
 
-            return $rows;
+                return $rows;
+
+            } catch (\Throwable $th) {
+                throw new \Exception("{$th->getMessage()}", 1);
+            }
         }
 
         public function updateRegistry(string $table, string $user_name, string $email, string $id_user, object $dbcon)
         {
             $query = "UPDATE $table SET user_name = :user_name, email = :email WHERE id_user = :id_user";                 
-
-            $stm = $dbcon->pdo->prepare($query); 
-            $stm->bindValue(":user_name", $user_name);				
-            $stm->bindValue(":email", $email);
-            $stm->bindValue(":id_user", $id_user);              
-            $stm->execute();       				
-            $stm->closeCursor();
-            $dbcon = null;            
+                        
+            try {
+                $stm = $dbcon->pdo->prepare($query); 
+                $stm->bindValue(":user_name", $user_name);				
+                $stm->bindValue(":email", $email);
+                $stm->bindValue(":id_user", $id_user);              
+                $stm->execute();       				
+                $stm->closeCursor();
+                
+            } catch (\Throwable $th) {
+                throw new \Exception("{$th->getMessage()}", 1);
+            }
         }
 
         public function updatePassword(string $table, string $password, string $id_user, object $dbcon)
         {
             $query = "UPDATE $table SET password = :password WHERE id_user = :id_user";                 
-
-            $stm = $dbcon->pdo->prepare($query); 
-            $stm->bindValue(":password", password_hash($password, PASSWORD_DEFAULT));				            
-            $stm->bindValue(":id_user", $id_user);              
-            $stm->execute();       				
-            $stm->closeCursor();
-            $dbcon = null;            
+                        
+            try {
+                $stm = $dbcon->pdo->prepare($query); 
+                $stm->bindValue(":password", password_hash($password, PASSWORD_DEFAULT));				            
+                $stm->bindValue(":id_user", $id_user);              
+                $stm->execute();       				
+                $stm->closeCursor();
+                
+            } catch (\Throwable $th) {
+                throw new \Exception("{$th->getMessage()}", 1); 
+            }
         }
 
         public function deleteRegistry(string $table, string $id_user, object $dbcon)
         {
-            $query = "DELETE FROM $table WHERE id_user = :id_user";                 
+            $query = "DELETE FROM $table WHERE id_user = :id_user";                             
+            
+            try {
+                $stm = $dbcon->pdo->prepare($query);             			            
+                $stm->bindValue(":id_user", $id_user);              
+                $stm->execute();       				
+                $stm->closeCursor();
 
-            $stm = $dbcon->pdo->prepare($query);             			            
-            $stm->bindValue(":id_user", $id_user);              
-            $stm->execute();       				
-            $stm->closeCursor();
-            $dbcon = null;            
+            } catch (\Throwable $th) {
+                throw new \Exception("{$th->getMessage()}", 1); 
+            }
         }
 
         /**
