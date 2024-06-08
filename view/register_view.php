f6149c2a71bec392d92c0146144135a0870fd7b3
Mario <cursotecnoweb@gmail.com>
2024-06-08 09:41:17 +0200

    Updated 'Register' view

--- view/register_view.php
+++ view/register_view.php
@@ -18,20 +18,24 @@
             </div>
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label d-none" for="password">Password:</label>
-                <div class="col-sm-8 mx-auto">
+                <div class="col-sm-8 mx-auto d-flex">
                     <div class="col-10 float-start">
                         <input class="form-control password" type="password" name="password" id="password" value="<?php if(isset($fields['password'])) echo $fields['password']; ?>" placeholder="Password" required>
                     </div>                    
-                    <label class="d-inline-block col-1 ms-2 me-2 show_password" for="password"><img src="/images/eye.png" alt="eye" height="40"></label>
+                    <div class="col-1 col-md-1 d-flex justify-content-center align-items-center ps-0">
+                        <img class="show_password p-0" src="/images/eye.svg" alt="eye" height="20">
+                    </div>
                 </div>                
             </div>
             <div class="row mb-3">
                 <label class="col-sm-2 col-form-label d-none" for="repeat_password">Repeat Password:</label>
-                <div class="col-sm-8 mx-auto">
+                <div class="col-sm-8 mx-auto d-flex">
                     <div class="col-10 float-start">
                         <input class="form-control password" type="password" name="repeat_password" id="repeat_password" value="<?php if(isset($fields['repeat_password'])) echo $fields['repeat_password']; ?>" placeholder="Repeat Password" required>
                     </div>                    
-                    <label class="d-inline-block col-1 ms-2 me-2 show_password" for="repeat_password"><img src="/images/eye.png" alt="eye" height="40"></label>
+                    <div class="col-1 col-md-1 d-flex justify-content-center align-items-center ps-0">
+                        <img class="show_password p-0" src="/images/eye.svg" alt="eye" height="20">
+                    </div> 
                 </div>                
             </div>
             <div class="row mb-3">
