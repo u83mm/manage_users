d8cf62fb4cf7b45efc32c9ff2e6949c7e54a7514
Mario <cursotecnoweb@gmail.com>
2024-06-08 13:40:27 +0200

    Added some responsive style

--- view/admin/user_change_password.php
+++ view/admin/user_change_password.php
@@ -6,21 +6,27 @@
 	$home->do_html_header($home->title, $home->h1, $home->meta_name_description, $home->meta_name_keywords);
 	$home->do_html_nav($home->nav_links);
 ?>	
-    <div class="col-6 mx-auto credentials">
+    <div class="col-12 col-md-8 col-xl-6 mx-auto credentials">
         <h4>CHANGE PASSWORD</h4>
         <?php echo $message ?? ""; ?>
         <form action="<?php PATH ?>" method="post"> 
             <input type="hidden" name="id_user" value="<?php if(isset($fields['id_user'])) echo $fields['id_user']; ?>">           
             <div class="row mb-3">
                 <label class="col-sm-4 col-form-label" for="password">Password:</label>
-                <div class="col-sm-6">
+                <div class="col-10 col-sm-6">
                     <input class="form-control" type="password" name="password" id="password" value="<?php if(isset($fields['password'])) echo $fields['password']; ?>" required>
+                </div>
+                <div class="col-1 d-flex justify-content-center align-items-center ps-0">
+                    <img class="show_password p-0" src="/images/eye.svg" alt="eye" height="20">
                 </div>                
             </div>
             <div class="row mb-3">
                 <label class="col-sm-4 col-form-label" for="new_password">Repeat Password:</label>
-                <div class="col-sm-6">
+                <div class="col-10 col-sm-6">
                     <input class="form-control" type="password" name="new_password" id="new_password" value="<?php if(isset($fields['new_password'])) echo $fields['new_password']; ?>" required>
+                </div>
+                <div class="col-1 d-flex justify-content-center align-items-center ps-0">
+                    <img class="show_password p-0" src="/images/eye.svg" alt="eye" height="20">
                 </div>                
             </div>               
             <div class="row mb-3">
